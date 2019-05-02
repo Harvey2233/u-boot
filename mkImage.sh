ab83a1c222685094876ea7641d0c91de4c70677f
harvey.li <948250436@qq.com>
2019-05-02 05:22:44 -0700

    add make Image shell

---
+++ mkImage.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+make clean
+make
+./mkbl2 spl/u-boot-spl.bin bl2.bin 14336
+# bl1
+sudo dd iflag=dsync oflag=dsync if=E4412_N.bl1.bin of=/dev/sdb seek=1;
+# bl2
+sudo dd iflag=dsync oflag=dsync if=bl2.bin of=/dev/sdb seek=17;
+# uboot
+sudo dd iflag=dsync oflag=dsync if=u-boot.bin of=/dev/sdb seek=50;
+
+sync
